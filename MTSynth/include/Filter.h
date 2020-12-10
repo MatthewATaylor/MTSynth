65d43de6643a00a3e41ffeafc85d10a6cb9162d4
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2020-12-09 22:32:26 -0500

    Add Filter class

---
+++ MTSynth/include/Filter.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <chrono>
+#include <stdexcept>
+
+class Filter {
+private:
+	unsigned int prevInput = 0;
+	unsigned int prevHighPassOutput = 0;
+	unsigned int prevLowPassOutput = 0;
+
+	std::chrono::steady_clock::time_point prevTime = std::chrono::steady_clock::now();
+
+public:
+	enum class Type {
+		LOW_PASS, HIGH_PASS, BAND_PASS
+	};
+
+	unsigned int get(unsigned int input, double rc, Type type);
+};
---
+++ MTSynth/source/Filter.cpp
@@ -0,0 +1,41 @@
+#include "Filter.h"
+
+unsigned int Filter::get(unsigned int input, double rc, Type type) {
+	auto currentTime = std::chrono::steady_clock::now();
+	double elapsedTime_s =
+		std::chrono::duration_cast<std::chrono::duration<double, std::chrono::seconds>>(
+			currentTime - prevTime
+		).count();
+
+	// High-pass RC filter -> low-pass RC filter
+
+	// High-pass filter component
+	double highPassAlpha = rc / (rc + elapsedTime_s);
+	unsigned int highPassOutput = highPassAlpha * prevHighPassOutput +
+		highPassAlpha * (input - prevInput);
+
+	// Low-pass filter component
+	double lowPassAlpha = elapsedTime_s / (rc + elapsedTime_s);
+	unsigned int lowPassOutput = lowPassAlpha * highPassOutput +
+		(1 - lowPassAlpha) * prevLowPassOutput;
+
+	prevInput = input;
+	prevHighPassOutput = highPassOutput;
+	prevLowPassOutput = lowPassOutput;
+
+	prevTime = currentTime;
+
+	switch (type) {
+	case Type::LOW_PASS:
+		return input - highPassOutput;
+		break;
+	case Type::HIGH_PASS:
+		return highPassOutput - lowPassOutput;
+		break;
+	case Type::BAND_PASS:
+		return lowPassOutput;
+		break;
+	}
+
+	throw std::exception("Unimplemented filter type");
+}
